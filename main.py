2ca9eab85e920eca699fa980b13fb5e6b980495c
Adamu Abubakar <83482261+scientadamu@users.noreply.github.com>
2022-06-14 14:24:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import random
-user_input = input("Enter a choice (R, P, S): ")
-possible_actions = ["R", "P", "S"]
-cpu_option = random.choice(possible_actions)
-print(f"\nPlayer {user_input}, CPU {cpu_option}.\n")
+while True:
+    posible_choices=["R","S","P"]
+    player_choice = None
+    system_choice = random.choice(posible_choices)
+    while player_choice not in posible_choices:
+        player_choice = input("please choice R for Rock, or S for sciessors or P for paper : ").upper()
+    if player_choice == system_choice:
+        print(f"\nplayer ({player_choice}), \nsystem ({system_choice})")
+        print(f"\nTies! both player and system choiced ({player_choice})\n")
+        player_choice = input("please choice R for Rock, or S for sciessors or P for paper : ").upper()
 
-if user_input == cpu_option:
-    print(f"Both players selected {user_input}. It's a tie!")
-elif user_input == "R":
-    if cpu_option == "S":
-        print("ROCK smashes SCISSORS! You win!")
-    else:
-        print("PAPER covers ROCK! You lose.")
-elif user_input == "PAPER":
-    if cpu_option == "R":
-        print("P covers ROCK! You win!")
-    else:
-        print("SCISSORS cuts PAPER! You lose.")
-elif user_input == "S":
-    if cpu_option == "P":
-        print("SCISSORS cuts PAPER! You win!")
-    else:
-        print("ROCK smashes SCISSORS! You lose.")
+    elif player_choice == "R":
+        if system_choice == "P":
+            print(f"\nplayer ({player_choice}), \nsystem ({system_choice})")
+            print(f"\nYou lose! paper cover Rock.\n")
+
+        if system_choice == "S":
+            print(f"\nplayer ({player_choice}), \nsystem ({system_choice})")
+            print(f"\nYou win! Rock smatch sciessors.\n")
+            
+    elif player_choice == "S":
+        if system_choice == "P":
+            print(f"\nplayer ({player_choice}), \nsystem ({system_choice})")
+            print(f"\nYou WIN! Sciessors smatch paper.\n")
+
+        if system_choice == "R":
+            print(f"\nplayer ({player_choice}), \nsystem ({system_choice})")
+            print(f"\nYou Lose! Rock smatch sciessors.\n")
+            
+    elif player_choice == "P":
+        if system_choice == "R":
+            print(f"\nplayer ({player_choice}), \nsystem ({system_choice})")
+            print(f"\nYou WIN! Paper Cover Rock.\n")
+
+        if system_choice == "S":
+            print(f"\nplayer ({player_choice}), \nsystem ({system_choice})")
+            print(f"\nYou Lose! Sciessors cut paper.\n")
+    play_again=input("Do you want to play again (y/n)")
+    
+    if play_again !="y":
+        break
+print("thank you for playing, See you next Time")
